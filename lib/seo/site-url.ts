5f983cf237b104909d6bbcc14910a2fcb5d814da
mauloajin <mauloa32153@gmail.com>
2026-06-18 14:24:20 +0900

    Publish Ochiizumibeya static page

--- lib/seo/site-url.ts
+++
@@ -1,7 +0,0 @@
-export function getSiteUrl(): string {
-  if (process.env.NEXT_PUBLIC_SITE_URL) {
-    return process.env.NEXT_PUBLIC_SITE_URL.replace(/\/$/, "");
-  }
-
-  return "https://akasaka-kyosuke.vercel.app";
-}
